d1cdfcbbb6f2118b4ce5869d5b41e1c6c9573de1
Daniel Polo <daniel_polo@live.com>
2021-04-10 01:05:23 -0300

    Criado o Projeto Principal

---
+++ PontoEletronicoWeb/Client/Shared/Componentes/SubMenuSuperior.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PontoEletronicoWeb.Client.Shared.Componentes
+{
+    public class SubMenuSuperiorBase : ComponentBase
+    {
+        #region Propriedades
+        protected string DataAtual { get; set; }
+        #endregion
+
+        #region Contrutor
+        public SubMenuSuperiorBase() =>
+            DataAtual = DateTime.Now.ToLongDateString();
+        #endregion
+    }
+}
