f169cb3a8286bc33772d390bc35fb26c90b4c49b
Daniel Polo <daniel_polo@live.com>
2021-04-10 19:27:32 -0300

    Inici da Implementação do Layout Base

---
+++ PontoEletronicoWeb/Client/Shared/Componentes/PageConsulta.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PontoEletronicoWeb.Client.Shared.Componentes
+{
+    public class PageConsultaBase : ComponentBase
+    {
+        [Parameter]
+        public bool DescTipoBotao { get; set; } = true;
+
+        [Parameter]
+        public string NomeTela { get; set; }
+
+        [Parameter]
+        public string RotaHome { get; set; }
+
+    }
+}
