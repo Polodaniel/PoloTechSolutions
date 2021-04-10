658c998a2cf7ee1c892840db2d173ab81d26d2bd
Daniel Polo <daniel_polo@live.com>
2021-04-10 00:43:34 -0300

    Adicionar arquivos de projeto.

---
+++ PontoEletronicoWeb/Client/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PontoEletronicoWeb.Client.Pages
+{
+    [Authorize]
+    public class IndexBase : ComponentBase
+    {
+    }
+}
